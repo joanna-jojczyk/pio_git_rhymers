8f838f6ded5f24fb44b385a178a52c73a4454314
Jojczyk <jojczyk@p.lodz.pl>
2020-11-24 10:31:49 +0100

    punkt 1.3.7 zmiana polna NUMBERS na numbers

--- src/main/java/edu/kis/vh/nursery/defaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/defaultCountingOutRhymer.java
@@ -2,33 +2,33 @@ package edu.kis.vh.nursery;
 
 public class defaultCountingOutRhymer {
 
-    private int[] NUMBERS = new int[12];
+    private int[] numbers = new int[12];
 
-    public int total = -1;
+    public int currentNumberIndex = -1;
 
     public void countIn(int in) {
         if (!isFull())
-            NUMBERS[++total] = in;
+            numbers[++currentNumberIndex] = in;
     }
 
     public boolean callCheck() {
-        return total == -1;
+        return currentNumberIndex == -1;
     }
 
     public boolean isFull() {
-        return total == 11;
+        return currentNumberIndex == 11;
     }
 
     protected int peekaboo() {
         if (callCheck())
             return -1;
-        return NUMBERS[total];
+        return numbers[currentNumberIndex];
     }
 
     public int countOut() {
         if (callCheck())
             return -1;
-        return NUMBERS[total--];
+        return numbers[currentNumberIndex--];
     }
 
 }
